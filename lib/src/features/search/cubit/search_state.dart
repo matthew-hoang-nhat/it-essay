3ea2a349ffb7f111eee7b49fadac9abe817e39a4
levi <levihoang@github.com>
2022-11-15 09:19:13 +0700

    feature: search content

--- lib/src/features/search/cubit/search_state.dart
+++ lib/src/features/search/cubit/search_state.dart
@@ -6,20 +6,20 @@ abstract class SearchState extends Equatable {
     // required this.products,
     required this.categories,
     required this.isLoading,
-    required this.isEmpty,
+    // required this.isEmpty,
     // required this.isShowProducts,
   });
   final List<ContentSearch> contentSearches;
   // final List<Product> products;
   final List<Category> categories;
   final bool isLoading;
-  final bool isEmpty;
+  // final bool isEmpty;
   // final bool isShowProducts;
   @override
   List<Object> get props => [
         contentSearches,
         isLoading,
-        isEmpty,
+        // isEmpty,
         categories,
         // products,
         // isShowProducts,
@@ -31,7 +31,7 @@ class SearchInitial extends SearchState {
     required super.contentSearches,
     // required super.products,
     required super.isLoading,
-    required super.isEmpty,
+    // required super.isEmpty,
     required super.categories,
     // required super.isShowProducts,
   });
@@ -45,12 +45,12 @@ class NewSearchState extends SearchState {
     List<Category>? categories,
     bool? isLoading,
     bool? isShowProducts,
-    bool? isEmpty,
+    // bool? isEmpty,
   }) : super(
           contentSearches: contentSearches ?? oldState.contentSearches,
           // products: products ?? oldState.products,
           isLoading: isLoading ?? oldState.isLoading,
-          isEmpty: isEmpty ?? oldState.isEmpty,
+          // isEmpty: isEmpty ?? oldState.isEmpty,
           categories: categories ?? oldState.categories,
           // isShowProducts: isShowProducts ?? oldState.isShowProducts,
         );
