913ca3f40b2c4b54861a47d2844700dfd8dae27a
matthew <matthewhoangtrungnhat@github.com>
2022-12-15 11:09:20 +0700

    feature: logo and native splash screen

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:it_project/src/di/di_graph_setup.dart';
 
 Future<void> initializeApp() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await setupDependenciesGraph();
 }
