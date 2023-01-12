8301cccbedf415aef543ccfd90091a58eb302963
matthew <matthewhoangtrungnhat@github.com>
2023-01-12 13:56:16 +0700

    feature: refresh token

--- lib/src/utils/remote/services/dio_http_client.dart
+++ lib/src/utils/remote/services/dio_http_client.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:dio/native_imp.dart';
 import 'package:it_project/main.dart';
 import 'package:it_project/src/features/app/fuser_local.dart';
+import 'package:it_project/src/utils/repository/auth_repository_impl.dart';
 
 const String keyAccept = 'Accept';
 const String keyAuth = 'Authorization';
@@ -11,9 +12,29 @@ class DioHttpClient extends DioForNative {
     String baseUrl, {
     BaseOptions? options,
   }) : super(options) {
-    interceptors.add(InterceptorsWrapper(onRequest: _requestInterceptor));
-
+    interceptors.add(InterceptorsWrapper(
+        onRequest: _requestInterceptor, onError: _errorInterceptor));
     this.options.baseUrl = baseUrl;
+  }
+
+  final fUserLocal = getIt<FUserLocal>();
+  Future<void> _errorInterceptor(
+      DioError err, ErrorInterceptorHandler handler) async {
+    final authRepo = getIt<AuthRepositoryImpl>();
+
+    if (err.response?.statusCode == 409) {
+      final currentRefreshToken = fUserLocal.refreshToken;
+      fUserLocal.fUser =
+          fUserLocal.fUser?.copyWith(accessToken: currentRefreshToken);
+      final result = await authRepo.refreshToken();
+      if (result.isSuccess) {
+        final newAcceptToken = result.data?['access_token'];
+        final newRefreshToken = result.data?['refreshToken'];
+        fUserLocal.fUser = fUserLocal.fUser?.copyWith(
+            accessToken: newAcceptToken, refreshToken: newRefreshToken);
+      }
+    }
+    handler.next(err);
   }
 
   void _requestInterceptor(
